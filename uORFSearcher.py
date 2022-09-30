cf48ecec950277fbcef0f9a23a580e0f78b982b0
JC-therea <josemontanyesbq@gmail.com>
2022-09-30 13:23:25 +0200

    Some codons are detected as good ones but they are not, check and fix

--- uORFSearcher.py
+++ uORFSearcher.py
@@ -1,11 +1,14 @@
-%%writefile "Inputs/Scripts/uORFSearcher.py"
+# %%writefile "Inputs/Scripts/uORFSearcher.py"
 
 import sys
 from Bio import SeqIO
 import pandas as pd
-import numpy as np
-from Bio.Seq import Seq
-import glob
+
+try:
+    functionName, file, AA_min, outputPrefix = sys.argv
+except:
+    print("error: AnnotationQC.py <Input file> <Output prefix>")
+    quit()
 
 ############# Define OBJECTS ##############
 class orf_object:
@@ -31,7 +34,8 @@ def find_all_in_frame(sequence, subsequence):
         start = next_idx + 1  # Move past this on the next time around
         next_idx = sequence.find(subsequence, start)
 
-    return idxs
+    idxs_1_based = [x+1 for x in idxs]
+    return idxs_1_based
 
 def find_orfs_in_frame(sequence, threshold, name):
     """ Finds all valid open reading frames in the string 'sequence' (in frame), and
@@ -48,11 +52,20 @@ def find_orfs_in_frame(sequence, threshold, name):
         stop_amber = find_all_in_frame(sequence[frame:], 'TAG')
         stop_ochre = find_all_in_frame(sequence[frame:], 'TAA')
         stop_umber = find_all_in_frame(sequence[frame:], 'TGA')
-        starts = starts_a + starts_c + starts_g + starts_t
-        stops = stop_amber + stop_ochre + stop_umber
+
+        starts_a_gff_adjustment = [x + frame for x in starts_a]
+        starts_t_gff_adjustment = [x + frame for x in starts_t]
+        starts_c_gff_adjustment = [x + frame for x in starts_c]
+        starts_g_gff_adjustment = [x + frame for x in starts_g]
+
+        stop_amber_gff_adjustment = [x + frame for x in stop_amber]
+        stop_ochre_gff_adjustment = [x + frame for x in stop_ochre]
+        stop_umber_gff_adjustment = [x + frame for x in stop_umber]
+
+        starts = starts_a_gff_adjustment + starts_t_gff_adjustment + starts_c_gff_adjustment + starts_g_gff_adjustment
+        stops = stop_amber_gff_adjustment + stop_ochre_gff_adjustment + stop_umber_gff_adjustment
         starts.sort()
         stops.sort()
-
 
         orfs = []
 
@@ -73,7 +86,7 @@ def find_orfs_in_frame(sequence, threshold, name):
                     # Choose the bigger CDS
                     bigger_start = possible_starts_above_threshold[0]
 
-                    orf_obj = orf_object(sequence[bigger_start:stop + 3], bigger_start, stop + 3, name)
+                    orf_obj = orf_object(sequence[bigger_start:stop + 2], bigger_start, stop + 2, name)
                     orfs.append(orf_obj)
             else:
                 possible_starts = [start for start in starts if start < stop and (start - stop) % 3 == 0 and start > last_stop]
@@ -83,7 +96,7 @@ def find_orfs_in_frame(sequence, threshold, name):
                 if len(possible_starts_above_threshold) > 0:
                     possible_starts_above_threshold.sort()
                     bigger_start = possible_starts_above_threshold[0]
-                    orf_obj = orf_object(sequence[bigger_start:stop + 3], bigger_start, stop + 3, name)
+                    orf_obj = orf_object(sequence[bigger_start:stop + 2], bigger_start, stop + 2, name)
                     orfs.append(orf_obj)
 
         orfs.sort(key=lambda x: x.start, reverse=False)
@@ -99,6 +112,13 @@ def from_orfs_to_gff(orfDict_pos, orfDict_neg, fastaseq,firstORF = 0):
     totalEntries_neg = sum([len(orfList) for orfList in orfDict_neg.values()])
 
     chr = orfDict_pos[0][0].seqName
+
+    #PandasSeries
+
+    Start = []
+    End = []
+    AttrList = []
+
 
     # + strand
     gffPos = pd.DataFrame("x", index = range(totalEntries_pos), columns=colnames)
@@ -121,9 +141,13 @@ def from_orfs_to_gff(orfDict_pos, orfDict_neg, fastaseq,firstORF = 0):
     gffPos.start = startListPos
     gffPos.end = endListPos
 
+    Start.extend(startListPos)
+    End.extend(endListPos)
+
     ini = firstORF
     lastORF = ini + len(startListPos)
     attr = ['ID=ORF-' + str(i).zfill(8) for i in range(ini, lastORF)]
+    AttrList.extend(attr)
     gffPos.attr = attr
     # - strand
     gffNeg = pd.DataFrame("x", index = range(totalEntries_neg), columns=colnames)
@@ -139,22 +163,32 @@ def from_orfs_to_gff(orfDict_pos, orfDict_neg, fastaseq,firstORF = 0):
         endListOfListsNeg.append([o.start for o in orfDict_neg[frame]])
         startListOfListsNeg.append([o.end for o in orfDict_neg[frame]])
 
-    startListNeg = len(fastaseq) - pd.array(sum(startListOfListsNeg, []))
-    endListNeg = len(fastaseq) - pd.array(sum(endListOfListsNeg, []))
+    startListNeg = len(fastaseq) - pd.array(sum(startListOfListsNeg, [])) + 1
+    endListNeg = len(fastaseq) - pd.array(sum(endListOfListsNeg, [])) + 1
     gffNeg.start = startListNeg
     gffNeg.end = endListNeg
-    ini = lastORF + 1
+
+    Start.extend(startListNeg)
+    End.extend(endListNeg)
+
+    ini = lastORF
     lastORF = ini + len(startListNeg)
     attr = ['ID=ORF-' + str(i).zfill(8) for i in range(ini, lastORF)]
+    AttrList.extend(attr)
     gffNeg.attr = attr
-    gff = pd.concat([gffPos, gffNeg])
-    gff.index = range(len(startListPos) + len(startListNeg))
 
-    return(gff)
+    chrList = [chr] * len(Start)
+    source = ["ORFSearcher"] * len(Start)
+    feature = ["ORF"] * len(Start)
+    score = ["."] * len(Start)
+    frame = ["."] * len(Start)
+    Strand = ["+"] * len(startListPos) + ["-"] * len(startListNeg)
 
+    #gff = pd.concat([gffPos, gffNeg])
+    #gff.index = range(len(startListPos) + len(startListNeg))
 
-
-
+    #return(gff)
+    return(chrList, source, feature, Start, End, score, Strand, frame, AttrList, lastORF)
 
 
 ######################################################
@@ -162,87 +196,61 @@ def from_orfs_to_gff(orfDict_pos, orfDict_neg, fastaseq,firstORF = 0):
 ######################################################
 
 # Input
-fasta = "Inputs/TestTRANSCRIPTS.fa"
+fasta = file
+outFile = outputPrefix + "_ORFs.gff3"
 # Read the input as fasta
 fastaIO = SeqIO.index(fasta, "fasta")
 output = "potato.gff"
 
 # Here we change aa threshold
-threshold = 16
+threshold = AA_min
 #############################
+colnames = ["chr", "source", "feature", "start", "end", "score", "strand", "frame", "attr"]
 
+chrList = []
+sourceList = []
+featureList = []
+startList = []
+endList = []
+scoreList = []
+strandList = []
+frameList = []
+attrList = []
+lastORF = 0
 for sequence in fastaIO:
-    orfDict_Pos = find_orfs_in_frame(fastaIO[sequence].seq, threshold, sequence)
-    orfDict_Neg = find_orfs_in_frame(fastaIO[sequence].seq, threshold, sequence)
+    print("Starting ORF search in " + sequence + "...")
+    orfDict_Pos = find_orfs_in_frame(str(fastaIO[sequence].seq).upper(), threshold, sequence)
+    print("50 % done in " + sequence)
+    orfDict_Neg = find_orfs_in_frame(str(fastaIO[sequence].seq.reverse_complement()).upper(), threshold, sequence)
+    print("All ORFs in " + sequence + " found, finishing last details...")
+    chr, source, feature, start, end, score, strand, frame, attr, lastORF = from_orfs_to_gff(orfDict_Pos,orfDict_Neg, fastaIO[sequence].seq, lastORF)
+    chrList.extend(chr)
+    sourceList.extend(source)
+    featureList.extend(feature)
+    startList.extend(start)
+    endList.extend(end)
+    scoreList.extend(score)
+    strandList.extend(strand)
+    frameList.extend(frame)
+    attrList.extend(attr)
+    print(sequence + " done!")
 
+gff = pd.DataFrame("x", index = range(len(chrList)), columns=colnames)
+gff.chr = chrList
+gff.source = sourceList
+gff.feature = featureList
+gff.start = startList
+gff.end = endList
+gff.score = scoreList
+gff.strand = strandList
+gff.frame = frameList
+gff.attr = attrList
 
+gffSorted = gff.sort_values(["chr", "start"], ascending = [True, True])
+gffSorted.attr = ['ID=ORF-' + str(i).zfill(8) for i in range(0, len(gff.attr))]
+gffSorted.to_csv(outFile, sep="\t", index=False, header=False)
 
-for sequence in fastaIO:
-    if sequence:
-        orfs_1[sequence] = find_orfs_in_frame(fastaIO[sequence].seq, threshold)
-for sequence in fastaIO:
-    if sequence:
-        orfs_2[sequence] = find_orfs_in_frame(fastaIO[sequence].seq[1:], threshold)
-for sequence in fastaIO:
-    if sequence:
-        orfs_3[sequence] = find_orfs_in_frame(fastaIO[sequence].seq[2:], threshold)
-
-print("Loading ORFs from " + yeast)
-CDS_dictionary = {}
-
-for transcript, ORF_objects in orfs_1.items():
-    if transcript:
-        longest_ORF_sequence = ''
-        longest_ORF_length = 0
-        n = 0
-        for single_ORF_object in ORF_objects:
-            n += 1
-            longest_ORF_sequence = single_ORF_object.sequence
-            transcript_add = transcript + "_" + str(n) + "-frame_1"
-            CDS_dictionary[transcript_add] = longest_ORF_sequence
-    else:
-        pass
-
-for transcript, ORF_objects in orfs_2.items():
-    if transcript:
-        longest_ORF_sequence = ''
-        longest_ORF_length = 0
-        n = 0
-        for single_ORF_object in ORF_objects:
-            n += 1
-            longest_ORF_sequence = single_ORF_object.sequence
-            transcript_add = transcript + "_" + str(n) + "-frame_2"
-            CDS_dictionary[transcript_add] = longest_ORF_sequence
-    else:
-        pass
-
-for transcript, ORF_objects in orfs_3.items():
-    if transcript:
-        longest_ORF_sequence = ''
-        longest_ORF_length = 0
-        n = 0
-        for single_ORF_object in ORF_objects:
-            n += 1
-            longest_ORF_sequence = single_ORF_object.sequence
-            transcript_add = transcript + "_" + str(n) + "-frame_3"
-            CDS_dictionary[transcript_add] = longest_ORF_sequence
-    else:
-        pass
-stored_ORFs = open(output, 'w+')
-print("Wrote " + str(len(CDS_dictionary)) + " CDSs")
-
-for sequence in CDS_dictionary:
-    fasta_index = ">" + sequence + "\n"
-    stored_ORFs.write(fasta_index)
-    nucleotide_sequence = str(CDS_dictionary[sequence].translate()) + "\n"
-    stored_ORFs.write(nucleotide_sequence)
-
-stored_ORFs.close()
-
-
-fastaFile = "Inputs/TestTRANSCRIPTS.fa"
-fasta_sequences = SeqIO.parse(open(fastaFile),'fasta')
-fasta = SeqIO.index(open(fastaFile),'fasta')
-
-for fasta in fasta_sequences:
-    name, sequence = fasta.id, str(fasta.seq)
+# Test
+#fasta = "Inputs/TestTRANSCRIPTS.fa"
+#fastaIO = SeqIO.index(fasta, "fasta")
+#threshold = 25
