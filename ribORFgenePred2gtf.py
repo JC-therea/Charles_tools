2ca48db60f75cf4f44f1c2bde11f9986b32fc81c
JC-therea <josemontanyesbq@gmail.com>
2023-02-03 15:34:02 +0100

    Working on it to produce better outputs

--- ribORFgenePred2gtf.py
+++ ribORFgenePred2gtf.py
@@ -1,21 +1,31 @@
 
 import sys
 
-try:
-    functionName, filePath, featureChosen, IGV_mode, outFilePath = sys.argv
-except:
-    print("error: ribORFgenePred2gtf.py <ribORF genePred file> <ribORF features splitted by comma or All> <IGV mode (True or False)> <Output file>")
-    quit()
+# try:
+#     functionName, filePath, featureChosen, IGV_mode, outFilePath = sys.argv
+# except:
+#     print("error: ribORFgenePred2gtf.py <ribORF genePred file> <ribORF features splitted by comma or All> <IGV mode (True or False)> <Output file>")
+#     quit()
+#
+# featureChosenList = featureChosen.split(",")
 
-featureChosenList = featureChosen.split(",")
+# Modificar esta parte para el cambio manual
+filePath = "candidateORF.genepred.txt"
+featureChosen = ["All"]
+IGV_mode = "True"
+outFilePath = "candidateORF.gtf"
 for feature in featureChosenList:
     if feature not in ["All","canonical","extension","odORF","iORF","noncoding","ouORF","dORF","readthrough","truncation","uORF"]:
         print("error: Not correct feature type " + feature)
         quit()
 
 ORF_info_path = filePath
-outPath = outFilePath + ".gtf"
+outPath = outFilePath
 
+### Esto tambien es nuevoooo
+transcriptsDict = {}
+
+###
 # Get coordinates in sequence
 outFile = open(outPath, "w+")
 with open(ORF_info_path, "r") as file:
@@ -27,18 +37,28 @@ with open(ORF_info_path, "r") as file:
        ORF = geneID + "_ORFID=" + nORF
 
        if IGV_mode == "True":
+           # Esto es lo que ha cambiado, lo que no es IGV apenas se ha tcoado
+           if geneID not in transcriptsDict.keys():
+               transcriptsDict[geneID] = 1
+               transcript = 0
+           else:
+               transcript = transcriptsDict[geneID]
+               transcriptsDict[geneID] += 1
            ORFexon = 1
-           attribute_out = 'gene_id "' + geneID.split("-T")[0] + '"; transcript_id "' + geneID + '"; ID "' + ORF + "-" + str(ORFexon) + '"; Parent "' + geneID + '";'
+           attribute_out = 'gene_id "' + geneID.split("-T")[0] + '"; transcript_id "' + geneID + "-TORF=" + str(transcript) + '"; ID "' + ORF + '"; Parent "' + geneID + '";'
        else:
            attribute_out = 'gene_id "' + geneID.split("-T")[0] + '"; transcript_id "' + geneID + '"; ID "' + ORF + '"; Parent "' + geneID + '";'
        NewORFstart = int(ORFstart) + 1
        if ORFType in featureChosenList or "All" in featureChosenList:
            if int(exonNumber) == 1:
-               CDS_text = chr + '\t' + "ribORF" + '\t' + "ORF" + '\t' + str(NewORFstart) + '\t' + ORFend + '\t' + '.' + '\t' + ribORFstrand + '\t' + "0" + '\t' + attribute_out + "\n"
+               # Aqui se ha añadido la linea transcript text
+               transcriptText = chr + '\t' + "ribORF" + '\t' + "transcript" + '\t' + exonStarts.split(",")[0] + '\t' + exonEnds.split(",")[0] + '\t' + '.' + '\t' + ribORFstrand + '\t' + "0" + '\t' + attribute_out + "\n"
+               outFile.writelines(transcriptText)
+               CDS_text = chr + '\t' + "ribORF" + '\t' + "CDS" + '\t' + str(NewORFstart) + '\t' + ORFend + '\t' + '.' + '\t' + ribORFstrand + '\t' + "0" + '\t' + attribute_out + "\n"
                outFile.writelines(CDS_text)
 
            else:
-               CDS_text = chr + '\t' + "ribORF" + '\t' + "ORF" + '\t' + str(NewORFstart) + '\t' + ORFend + '\t' + '.' + '\t' + ribORFstrand + '\t' + "0" + '\t' + attribute_out + "\n"
+               CDS_text = chr + '\t' + "ribORF" + '\t' + "CDS" + '\t' + str(NewORFstart) + '\t' + ORFend + '\t' + '.' + '\t' + ribORFstrand + '\t' + "0" + '\t' + attribute_out + "\n"
                ORF = geneID + "_ORFID=" + nORF
                exonStarts_List = exonStarts.split(",")
                exonEnds_List = exonEnds.split(",")
