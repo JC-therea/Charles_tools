15a6043d8f545695358b420841676cf70e3b118d
JC-therea <jmontanes@imim.es>
2023-02-21 14:35:36 +0100

    Improved AnnotationQC, gff format fixed

--- AnnotationQC.py
+++ AnnotationQC.py
@@ -14,7 +14,7 @@ except:
     print("error: AnnotationQC.py <Input file> <Output file>")
     #quit()
 
-#file = "/home/jmontanes/Documents/EvolutionNanopore/Inputs/Park_2014/editing/Scer_Park2014.gtf"
+#file = "/projects_eg/projects/jmontanes/EvolutionNanopore/Annotation/Tests/UpdateOnly-WorkWithChapuza/ScerBloom/update_results/Saccharomyces_cerevisiaeJC.gff3"
 #output = "Result.tsv"
 
 colnames = ["chr","source","feature","start","end","score","strand","frame","attr"]
@@ -27,6 +27,11 @@ if file.split(".")[-1] == "gff" or file.split(".")[-1] == "gff3":
     IDreplacePost = ";.*"
     genereplacePre = ".*Parent="
     genereplacePost = ";.*"
+
+    IDreplacePreNoTransc = ".*Parent="
+    IDreplacePostNoTransc = ";.*"
+    genereplacePreNoTransc = ".*Parent="
+    genereplacePostNoTransc = "-T.;.*"
     
 elif file.split(".")[-1] == "gtf":
 
@@ -34,6 +39,11 @@ elif file.split(".")[-1] == "gtf":
     IDreplacePost = '";.*'
     genereplacePre = 'gene_id "'
     genereplacePost = '";.*'
+
+    IDreplacePreNoTransc = '.*transcript_id "'
+    IDreplacePostNoTransc = '";.*'
+    genereplacePreNoTransc = 'gene_id "'
+    genereplacePostNoTransc = '";.*'
 
 else:
     
@@ -43,41 +53,33 @@ else:
     
 df = pd.read_csv(file, sep = "\t", comment = "#", names = colnames, header = None)
 transcripts = df[df.feature.isin(transcriptTypes)]
-transcripts['ID'] = transcripts["attr"].str.replace(IDreplacePre,'').str.replace(IDreplacePost,"")
-transcripts['gene'] = transcripts["attr"].str.replace(genereplacePre,'').str.replace(genereplacePost,"")
-mRNASize = transcripts.end.subtract(transcripts.start) + 1 
+transcripts = transcripts.assign(ID = transcripts["attr"].str.replace(IDreplacePre,'',regex=True).str.replace(IDreplacePost,"",regex=True))
+transcripts = transcripts.assign(gene = transcripts["attr"].str.replace(genereplacePre,'',regex = True).str.replace(genereplacePost,"", regex = True))
+
+mRNASize = transcripts.end.subtract(transcripts.start)
 mRNASize = mRNASize.set_axis(transcripts.ID)
 mRNASize.name = "mRNA_size"
 # Calculate CDS region
 CDS = df[df.feature.isin(["CDS"])]
-CDS['ID'] = CDS["attr"].str.replace(IDreplacePre,'').str.replace(IDreplacePost,"")
-CDS['gene'] = CDS["attr"].str.replace(genereplacePre,'').str.replace(genereplacePost,"")
-
-CDS["Size"] = CDS.end.subtract(CDS.start) + 1 
-CDSsum = CDS.groupby('gene')['Size'].transform("sum")
+CDS = CDS.assign(ID=CDS["attr"].str.replace(IDreplacePreNoTransc, '', regex=True).str.replace(IDreplacePostNoTransc, "", regex=True))
+CDS = CDS.assign(gene=CDS["attr"].str.replace(genereplacePreNoTransc, '', regex=True).str.replace(genereplacePostNoTransc, "",regex=True))
+CDS["Size"] = CDS.end.subtract(CDS.start)
+CDSsum = CDS.groupby('ID')['Size'].transform("sum")
 CDSSize = CDSsum.set_axis(CDS.ID)
 CDSSize.name = "CDS_size"
 CDSSize = CDSSize[~CDSSize.index.duplicated(keep='first')]
+
 #########################################################################
 # Calculate exon size
 exon = df[df.feature.isin(["exon"])]
-exon['ID'] = exon["attr"].str.replace(IDreplacePre,'').str.replace(IDreplacePost,"")
-exon['gene'] = exon["attr"].str.replace(genereplacePre,'').str.replace(genereplacePost,"")
+exon = exon.assign(ID=exon["attr"].str.replace(IDreplacePreNoTransc, '', regex=True).str.replace(IDreplacePostNoTransc, "", regex=True))
+exon = exon.assign(gene=exon["attr"].str.replace(genereplacePreNoTransc, '', regex=True).str.replace(genereplacePostNoTransc, "",regex=True))
+exon["Size"] = exon.end.subtract(exon.start)
 
-exon["Size"] = exon.end.subtract(exon.start) + 1 
-if file.split(".")[-1] == "gff" or file.split(".")[-1] == "gff3":
-    
-    exonsum = exon.groupby('gene')['Size'].transform("sum")
-    exonSize = exonsum.set_axis(exon.ID)
-    exonSize.name = "Exon_size"
-    exonSize = exonSize[~exonSize.index.duplicated(keep='first')]
-    
-else:
-    
-    exonsum = exon.groupby('ID')['Size'].transform("sum")
-    exonSize = exonsum.set_axis(exon.ID)
-    exonSize.name = "Exon_size"
-    exonSize = exonSize[~exonSize.index.duplicated(keep='first')]
+exonsum = exon.groupby('ID')['Size'].transform("sum")
+exonSize = exonsum.set_axis(exon.ID)
+exonSize.name = "Exon_size"
+exonSize = exonSize[~exonSize.index.duplicated(keep='first')]
 
     
 # Check if there is some UTR annotated or not
@@ -86,9 +88,9 @@ if len(df[df.feature.isin(UTRTypes)]) == 0:
     UTRSeries = UTRSeries.set_axis(transcripts.ID)
 else:
     UTRs = df[df.feature.isin(UTRTypes)]
-    UTRs['ID'] = UTRs["attr"].str.replace(IDreplacePre,'').str.replace(IDreplacePost,"")
-    UTRs['gene'] = UTRs["attr"].str.replace(genereplacePre,'').str.replace(genereplacePost,"")
-    UTRs["Size"] = UTRs.end.subtract(UTRs.start) + 1
+    UTRs = UTRs.assign(ID=UTRs["attr"].str.replace(IDreplacePreNoTransc, '', regex=True).str.replace(IDreplacePostNoTransc, "", regex=True))
+    UTRs = UTRs.assign(gene=UTRs["attr"].str.replace(genereplacePreNoTransc, '', regex=True).str.replace(genereplacePostNoTransc, "",regex=True))
+    UTRs["Size"] = UTRs.end.subtract(UTRs.start)
     UTRsum = UTRs.groupby('gene')['Size'].transform("sum")
     UTRSeries = UTRsum.set_axis(UTRs.ID)
     UTRSeries.name = "UTR_size"
@@ -99,9 +101,9 @@ if len(df[df.feature.isin(["five_prime_UTR", "five_prime_utr"])]) == 0:
     UTR5Series = UTR5Series.set_axis(transcripts.ID)
 else:
     UTR5s = df[df.feature.isin(["five_prime_UTR", "five_prime_utr"])]
-    UTR5s['ID'] = UTR5s["attr"].str.replace(IDreplacePre,'').str.replace(IDreplacePost,"")
-    UTR5s['gene'] = UTR5s["attr"].str.replace(genereplacePre,'').str.replace(genereplacePost,"")
-    UTR5s["Size"] = UTR5s.end.subtract(UTR5s.start) + 1
+    UTR5s = UTR5s.assign(ID=UTRs["attr"].str.replace(IDreplacePreNoTransc, '', regex=True).str.replace(IDreplacePostNoTransc, "", regex=True))
+    UTR5s = UTR5s.assign(gene=UTRs["attr"].str.replace(genereplacePreNoTransc, '', regex=True).str.replace(genereplacePostNoTransc, "",regex=True))
+    UTR5s["Size"] = UTR5s.end.subtract(UTR5s.start)
     UTR5sum = UTR5s.groupby('gene')['Size'].transform("sum")
     UTR5Series = UTR5sum.set_axis(UTR5s.ID)
     UTR5Series.name = "UTR5_size"
@@ -112,9 +114,9 @@ if len(df[df.feature.isin(["three_prime_UTR", "three_prime_utr"])]) == 0:
     UTR3Series = UTR3Series.set_axis(transcripts.ID)
 else:
     UTR3s = df[df.feature.isin(["three_prime_UTR", "three_prime_utr"])]
-    UTR3s['ID'] = UTR3s["attr"].str.replace(IDreplacePre,'').str.replace(IDreplacePost,"")
-    UTR3s['gene'] = UTR3s["attr"].str.replace(genereplacePre,'').str.replace(genereplacePost,"")
-    UTR3s["Size"] = UTR3s.end.subtract(UTR3s.start) + 1
+    UTR3s = UTR3s.assign(ID=UTRs["attr"].str.replace(IDreplacePreNoTransc, '', regex=True).str.replace(IDreplacePostNoTransc, "", regex=True))
+    UTR3s = UTR3s.assign(gene=UTRs["attr"].str.replace(genereplacePreNoTransc, '', regex=True).str.replace(genereplacePostNoTransc, "",regex=True))
+    UTR3s["Size"] = UTR3s.end.subtract(UTR3s.start)
     UTR3sum = UTR3s.groupby('gene')['Size'].transform("sum")
     UTR3Series = UTR3sum.set_axis(UTR3s.ID)
     UTR3Series.name = "UTR3_size"
